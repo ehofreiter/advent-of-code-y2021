469b4055e79a11c984a392ea4c596bf255706577
Eric W. Hofreiter <35861161+ehofreiter@users.noreply.github.com>
2021-12-19 12:51:55 -0600

    day 19 cleanup
    rearranged, renamed, and annotated my Day 19 solution for posterity

--- src/AdventOfCodeY2021/Common.hs
+++ src/AdventOfCodeY2021/Common.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module AdventOfCodeY2021.Common where
 
 import           Data.Foldable
@@ -31,6 +32,17 @@ mkHistogram = foldl' (\m x -> Map.insertWith (+) x 1 m) Map.empty
 -- fromList [('A',3),('B',2),('C',1)]
 unMkHistogram :: Map.Map a Int -> [a]
 unMkHistogram = concatMap (\(x, i) -> replicate i x) . Map.toList
+
+-- | Gives all ways to choose 2 items from a list, modulo order.
+-- >>> choose2 [4,5,2,8]
+-- [(4,5),(4,2),(4,8),(5,2),(5,8),(2,8)]
+-- >>> choose2 [1,1,1]
+-- [(1,1),(1,1),(1,1)]
+-- >>> map (\n -> ((length (choose2 [1..n]), n*(n-1)`div`2))) [0..10]
+-- [(0,0),(0,0),(1,1),(3,3),(6,6),(10,10),(15,15),(21,21),(28,28),(36,36),(45,45)]
+choose2 :: [a] -> [(a,a)]
+choose2 [] = []
+choose2 (x:xs) = map (x,) xs ++ choose2 xs
 
 bfs :: Ord n => (n -> [n]) -> Seq n -> Seq n
 bfs getAdjs initialQueue = loop initialSeen initialQueue Seq.empty
--- src/AdventOfCodeY2021/Day19.hs
+++ src/AdventOfCodeY2021/Day19.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TupleSections #-}
 module AdventOfCodeY2021.Day19 where
 
 import Control.Applicative
@@ -33,9 +32,10 @@ runWith filePath = do
       --Just (o, t, ol) = matchScanners s0 s1
       --coordSet = stepAll scannerMap
       --part1 = Set.size coordSet
-      (scannerPositions, sm') = stepAll scannerMap
+      (transforms, sm') = solveScannerMap scannerMap
+      scannerPositions = map snd $ Map.elems $ transforms
       part1 = Set.size $ Set.fromList $ concatMap Map.elems (Map.elems sm')
-      part2 = maximum $ map (uncurry manhattanDist) $ allPairs scannerPositions
+      part2 = maximum $ map (uncurry manhattanDist) $ choose2 scannerPositions
   --pure scannerMap
   --print part1
   print part2
@@ -43,70 +43,34 @@ runWith filePath = do
 manhattanDist :: V3 Int -> V3 Int -> Int
 manhattanDist u v = sum $ abs <$> u - v
 
-allPairs :: [a] -> [(a,a)]
-allPairs [] = []
-allPairs (x:xs) = map (x,) xs ++ allPairs xs
-
--- stepAll :: ScannerMap -> Set.Set (V3 Int)
--- stepAll sm =
---   loop initKnown initKnownCoords initSm
---   where
---     initKnown = Map.singleton 0 (identity, zero)
---     initKnownCoords = Set.fromList $ Map.elems $ sm Map.! 0
---     initDiffs = diffs (sm Map.! 0)
---     initSm = Map.delete 0 sm
---     loop known knownCoords sm =
---       if Map.size sm == 0
---       then knownCoords
---       else let (known', knownCoords', sm') = stepScanner (known, knownCoords, sm)
---            in  loop known' knownCoords' sm'
-
-stepAll :: ScannerMap -> ([V3 Int], ScannerMap)
-stepAll =
+-- | Takes the initial puzzle input and yields a scanner map with all scanner
+-- beacons transformed to be relative to scanner 0, as well as a map giving the
+-- transform that was applied to each scanner to get its coordinates from
+-- initial to solved. Starts by assuming scanner 0 is solved relative to itself,
+-- then each iterative step finds and solves one additional scanner.
+solveScannerMap :: ScannerMap -> (TransformMap, ScannerMap)
+solveScannerMap =
   loop initKnown
   where
     initKnown = Map.singleton 0 (identity, zero)
     loop known sm =
       if Map.size known == Map.size sm
-      then (map snd $ Map.elems known, sm)
+      then (known, sm)
       else let (known', sm') = stepScannerMap (known, sm)
            in  loop known' sm'
 
--- stepScannerDiff
---   :: (Map.Map Int (M33 Int, V3 Int), Set.Set (V3 Int), ScannerMap)
---   -> (Map.Map Int (M33 Int, V3 Int), Set.Set (V3 Int), ScannerMap)
--- stepScannerDiff (knownScanners, knownCoords, knownDiffs, sm) =
---   let (k1, (r, t, ol)) = head $ mapMaybe matchUnknown $ Map.toList sm
---       matchUnknown (k1, s) = case matchScanner knownCoords s of
---         Nothing -> Nothing
---         Just result -> Just (k1, result)
---       known' = Map.insert k1 (r, t) knownScanners
---       newCoords = Map.elems $ Map.map (\c -> r !* c ^+^ t) $ sm Map.! k1
---       knownCoords' = foldl' (flip Set.insert) knownCoords newCoords
---       sm' = Map.delete k1 sm
---   in (known', knownCoords', sm')
-
-stepScanner
-  :: (Map.Map Int (M33 Int, V3 Int), Set.Set (V3 Int), ScannerMap)
-  -> (Map.Map Int (M33 Int, V3 Int), Set.Set (V3 Int), ScannerMap)
-stepScanner (knownScanners, knownCoords, sm) =
-  let (k1, (r, t, ol)) = head $ mapMaybe matchUnknown $ Map.toList sm
-      matchUnknown (k1, s) = case matchScanner knownCoords s of
-        Nothing -> Nothing
-        Just result -> Just (k1, result)
-      known' = Map.insert k1 (r, t) knownScanners
-      newCoords = Map.elems $ Map.map (\c -> r !* c ^+^ t) $ sm Map.! k1
-      knownCoords' = foldl' (flip Set.insert) knownCoords newCoords
-      sm' = Map.delete k1 sm
-  in (known', knownCoords', sm')
-
-stepScannerMap
-  :: (Map.Map Int (M33 Int, V3 Int), ScannerMap)
-  -> (Map.Map Int (M33 Int, V3 Int), ScannerMap)
+-- | Finds the next unsolved scanner and solves its position and orientation
+-- relative to scanner 0. Adds the unsolved scanner to the list of known scanners
+-- along with its position and orientation, and updates its beacon coordinates
+-- in the scanner map to be relative to scanner 0.
+stepScannerMap :: (TransformMap, ScannerMap) -> (TransformMap, ScannerMap)
 stepScannerMap (knownScanners, sm) =
   let knownKeys = Map.keys knownScanners
       unknownKeys = Map.keys sm \\ knownKeys
       searchPairs = (,) <$> knownKeys <*> unknownKeys
+      -- ^ most time consuming bug: I had this as 'zip knownKeys unknownKeys',
+      -- which tries only a frustratingly unpredictable subset of scanner pairs
+      -- and makes you think you're crazy
       matchKeys k0 k1 = case matchScanners (sm Map.! k0) (sm Map.! k1) of
         Nothing -> Nothing
         Just result -> Just (k1, result)
@@ -115,10 +79,26 @@ stepScannerMap (knownScanners, sm) =
       sm' = Map.adjust (Map.map (\c -> r !* c ^+^ t)) k1 sm
   in (known', sm')
 
+-- Beacon ID -> beacon position coordinate
 type CoordMap = Map.Map Int (V3 Int)
+-- (Beacon ID A, Beacon ID B) -> position A - position B
 type DiffMap = Map.Map (Int, Int) (V3 Int)
+-- Scanner ID -> beacon coordinates
 type ScannerMap = Map.Map Int CoordMap
+-- Scanner ID -> (rotation matrix, translation vector)
+type TransformMap = Map.Map Int (M33 Int, V3 Int)
 
+-- | Check if two scanner coordinate maps have 12 matching beacons. Yields
+-- 'Nothing' if no match, else yields just the rotation matrix and translation
+-- vector that can be applied to the second coordinate map to make it in terms
+-- of the first. Also yields the set of coordinate diffs that matched, mainly
+-- for debugging.
+--
+-- First, for each scanner, take all its n*(n-1) beacon coordinate differences.
+-- Then compare the difference magnitudes between the two scanners to see if we
+-- get the expected number of matches (12*11 = 132 matching diffs for 12
+-- matching beacons). If so, find the one of 24 possible orientations that makes
+-- the diffs match.
 matchScanners :: CoordMap -> CoordMap -> Maybe (M33 Int, V3 Int, Overlap)
 matchScanners cs0 cs1 =
   let cs0Diffs = diffs cs0
@@ -127,21 +107,14 @@ matchScanners cs0 cs1 =
       cs1OrientDiffs = map (\o -> (o, Map.map (o !*) cs1Diffs')) orients
       overlaps = map (diffOverlap cs0Diffs <$>) cs1OrientDiffs
   in do
-    guard (Map.size cs1Diffs' >= 66)
-    (r, ol) <- find ((>= 66) . length . snd) $ overlaps
-    let ((c0,_), (c1,_), _) = head ol
-        t = cs0 Map.! c0 ^-^ r !* cs1 Map.! c1
-    pure (r, t, ol)
-
-matchScanner :: Set.Set (V3 Int) -> CoordMap -> Maybe (M33 Int, V3 Int, Overlap)
-matchScanner cset cs1 =
-  let cs0 = Map.fromList $ zip [0..] $ Set.toList cset
-      cs0Diffs = diffs cs0
-      cs1Diffs = diffs cs1
-      cs1Diffs' = refineDiffsBy cs0Diffs cs1Diffs
-      cs1OrientDiffs = map (\o -> (o, Map.map (o !*) cs1Diffs')) orients
-      overlaps = map (diffOverlap cs0Diffs <$>) cs1OrientDiffs
-  in do
+    -- NB: 66 = 12 choose 2. The guard/filter on >= 66 is a heuristic for when
+    -- we think we have 12 matching beacons. Because the diffs actually include
+    -- all *ordered* pairs (e.g. (a,b) as well as (b,a)), there will actually be
+    -- 132 matching diffs when 12 beacons match. But this still worked. Would be
+    -- interesting to see how changing this affects the efficiency. A higher
+    -- number rules out more false positives, but if there are no false positives
+    -- above a certain threshold (seems to be the case for 66), you can do a bit
+    -- less work for the true positives.
     guard (Map.size cs1Diffs' >= 66)
     (r, ol) <- find ((>= 66) . length . snd) $ overlaps
     let ((c0,_), (c1,_), _) = head ol
@@ -172,6 +145,9 @@ diffs vs = Map.fromList $ catMaybes $ f <$> Map.toList vs <*> Map.toList vs
     f (k0, v0) (k1, v1) | k0 == k1 = Nothing
                         | otherwise = Just ((k0, k1), v0 ^-^ v1)
 
+-- | Caches the 24 different non-reflecting 3d orthogonal rotations. Starts with
+-- all orthogonal rotations and filters out the ones that have determinant of
+-- -1, which are the ones that include a reflection.
 orients :: [M33 Int]
 orients = filter ((== 1) . det33) allOrients
   where
@@ -179,15 +155,8 @@ orients = filter ((== 1) . det33) allOrients
       let cols = [V3 1 0 0, V3 0 1 0, V3 0 0 1]
       [vx, vy, vz] <- permutations cols
       applyFlips (V3 vx vy vz)
-
--- permuteOrientations :: V3 Int -> [V3 Int]
--- permuteOrientations (V3 x y z) = do
---   [px,py,pz] <- permutations [x,y,z]
---   applyFlipsV (V3 px py pz)
-
-applyFlips v3 = map (v3 ^*) flips
-applyFlipsV v3 = map (v3 *) flips
-flips = V3 <$> [-1,1] <*> [-1,1] <*> [-1,1]
+    applyFlips v3 = map (v3 ^*) flips
+    flips = V3 <$> [-1,1] <*> [-1,1] <*> [-1,1]
 
 type Scanner = (Int, Map.Map Int (V3 Int))
 
@@ -204,3 +173,54 @@ readCoord :: String -> V3 Int
 readCoord s = V3 x y z
   where
     [x,y,z] = map read $ splitOn "," s
+{-
+----------------------------------------
+-- Graveyard of Failed Attempts
+----------------------------------------
+
+-- This was a failed attempt to fix the 'zip unknownKeys knownKeys' bug, which
+-- made 'stepScannerMap' fail to solve all scanners. Based on wording in one
+-- part of the example, I thought the puzzle was saying you might have to solve
+-- some scanners against all known beacons at once (I missed where it says every
+-- one can be solved by analyzing scanners pairwise). The worst part was that
+-- this solution works and gives the correct answer, but is prohibitively
+-- inefficient to solve the full puzzle.
+stepScannerDiff
+  :: (Map.Map Int (M33 Int, V3 Int), Set.Set (V3 Int), ScannerMap)
+  -> (Map.Map Int (M33 Int, V3 Int), Set.Set (V3 Int), ScannerMap)
+stepScannerDiff (knownScanners, knownCoords, sm) =
+  let (k1, (r, t, ol)) = head $ mapMaybe matchUnknown $ Map.toList sm
+      matchUnknown (k1, s) = case matchScanner knownCoords s of
+        Nothing -> Nothing
+        Just result -> Just (k1, result)
+      known' = Map.insert k1 (r, t) knownScanners
+      newCoords = Map.elems $ Map.map (\c -> r !* c ^+^ t) $ sm Map.! k1
+      knownCoords' = foldl' (flip Set.insert) knownCoords newCoords
+      sm' = Map.delete k1 sm
+  in (known', knownCoords', sm')
+
+-- Part of above failed attempt.
+matchScanner :: Set.Set (V3 Int) -> CoordMap -> Maybe (M33 Int, V3 Int, Overlap)
+matchScanner cset cs1 =
+  let cs0 = Map.fromList $ zip [0..] $ Set.toList cset
+      cs0Diffs = diffs cs0
+      cs1Diffs = diffs cs1
+      cs1Diffs' = refineDiffsBy cs0Diffs cs1Diffs
+      cs1OrientDiffs = map (\o -> (o, Map.map (o !*) cs1Diffs')) orients
+      overlaps = map (diffOverlap cs0Diffs <$>) cs1OrientDiffs
+  in do
+    guard (Map.size cs1Diffs' >= 66)
+    (r, ol) <- find ((>= 66) . length . snd) $ overlaps
+    let ((c0,_), (c1,_), _) = head ol
+        t = cs0 Map.! c0 ^-^ r !* cs1 Map.! c1
+    pure (r, t, ol)
+
+-- Different way of permuting orientations that I ended up not using.
+permuteOrientations :: V3 Int -> [V3 Int]
+permuteOrientations (V3 x y z) = do
+  [px,py,pz] <- permutations [x,y,z]
+  applyFlipsV (V3 px py pz)
+  where
+    applyFlipsV v3 = map (v3 *) flips
+    flips = V3 <$> [-1,1] <*> [-1,1] <*> [-1,1]
+-}
